7119260815b289146c1628e54e8eeda390982109
Danik <l13m@mail.ru>
2023-02-14 18:32:27 +0300

    dz

---
+++ zadacha 1/zadacha 1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+Random random = new Random();
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[n, m];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+
+    {
+        array[i, j] = random.Next(1, 100);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
